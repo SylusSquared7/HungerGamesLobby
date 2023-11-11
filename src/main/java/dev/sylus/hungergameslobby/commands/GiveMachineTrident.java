19d45c648ecf4893815d987b184ecc94fa8bbf3f
SylusSquared7 <sylussquared@proton.me>
2023-11-11 23:39:28 +0000

    Fixed an issue with the machine gun trident where it would not work twice

--- src/main/java/dev/sylus/hungergameslobby/commands/GiveMachineTrident.java
+++ src/main/java/dev/sylus/hungergameslobby/commands/GiveMachineTrident.java
@@ -7,7 +7,9 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -28,14 +30,22 @@ public class GiveMachineTrident implements CommandExecutor {
             Bukkit.getLogger().log(Level.WARNING, "Non player tried to execute a player only command");
             return true;
         }
+        sender.sendMessage(ChatColor.RED + "Trying to give a trident");
         ItemStack trident = new ItemStack(Material.TRIDENT);
         ItemMeta meta = trident.getItemMeta();
-        meta.setDisplayName("§9MACHINE GUNDENT");
+        meta.setDisplayName("§cMACHINE GUNDENT");
         List<String> lore = new ArrayList<>();
-        lore.add("§9Machine gun trident");
-        lore.add("§eHOLD RIGHT CLICK§f: shoots a LOT of tridents");
+        lore.add("§7A trident that has the power");
+        lore.add("§7of 4 tridents per second :)");
+        lore.add("§f");
+        lore.add("§e§lHOLD RIGHT CLICK§r§f: shoots a LOT of tridents");
         lore.add("§cADMIN WEAPON");
+
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         meta.setLore(lore);
+        trident.setItemMeta(meta);
+        trident.addUnsafeEnchantment(Enchantment.PIERCING, 100);
         ((Player) sender).getInventory().addItem(trident);
         return true;
     }
--- src/main/java/dev/sylus/hungergameslobby/events/TridentMachineGun.java
+++ src/main/java/dev/sylus/hungergameslobby/events/TridentMachineGun.java
@@ -3,6 +3,7 @@ package dev.sylus.hungergameslobby.events;
 import dev.sylus.hungergameslobby.HungerGamesLobby;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Trident;
@@ -16,32 +17,50 @@ import org.bukkit.util.Vector;
 public class TridentMachineGun extends BukkitRunnable implements Listener {
     Player player;
     HungerGamesLobby main;
-    public TridentMachineGun(HungerGamesLobby mainInstance){
+    boolean isRunning = false;
+
+    public TridentMachineGun(HungerGamesLobby mainInstance) {
         main = mainInstance;
     }
 
     @EventHandler
-    public void  onPlayerRightClick(PlayerInteractEvent event){
-        player = event.getPlayer();
+    public void onPlayerRightClick(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
 
-        if (event.getAction().toString().contains("RIGHT") && player.getInventory().getItemInMainHand().getType() == Material.TRIDENT
-                && player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals("§9MACHINE GUNDENT")){
-            this.runTaskTimer(main, 0, 5);
+        if (event.getAction().toString().contains("RIGHT") &&
+                player.getInventory().getItemInMainHand().getType() == Material.TRIDENT &&
+                player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals("§cMACHINE GUNDENT")) {
+
+            if (!isRunning) {
+                isRunning = true;
+                shootTridents(player);
+            }
         }
+    }
+
+    private void shootTridents(Player player) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (player.isHandRaised()) {
+                    Location location = player.getLocation();
+                    Vector direction = location.getDirection();
+                    ItemStack trident = new ItemStack(Material.TRIDENT);
+                    trident.addUnsafeEnchantment(Enchantment.LOYALTY, 1);
+
+                    Trident tridentEntity = (Trident) player.launchProjectile(Trident.class);
+                    player.playSound(player.getLocation(), Sound.ITEM_TRIDENT_THROW, 1, 1);
+                    tridentEntity.setVelocity(direction.multiply(2.0));
+                } else {
+                    isRunning = false;
+                    this.cancel();  // This cancels the task
+                }
+            }
+        }.runTaskTimer(main, 0, 5);
     }
 
     @Override
     public void run() {
-        if (player.isHandRaised()){
-            Location location = player.getLocation();
-            Vector direction = location.getDirection();
-            ItemStack trident = new ItemStack(Material.TRIDENT);
-            trident.addEnchantment(Enchantment.LOYALTY, 1);
 
-            Trident tridentEntity = (Trident) player.launchProjectile(Trident.class);
-            tridentEntity.setVelocity(direction.multiply(2.0));
-        } else {
-            cancel();
-        }
     }
 }
