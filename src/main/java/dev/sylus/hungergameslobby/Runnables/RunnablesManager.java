a41d69d5d3761f2aefd516560a9f761891275558
SylusSquared7 <sylussquared@proton.me>
2023-12-06 18:50:26 +0000

    Started working on the runnables

--- src/main/java/dev/sylus/hungergameslobby/Runnables/RunnablesManager.java
+++ src/main/java/dev/sylus/hungergameslobby/Runnables/RunnablesManager.java
@@ -1,12 +1,25 @@
 package dev.sylus.hungergameslobby.Runnables;
 
 import dev.sylus.hungergameslobby.game.Game;
+import dev.sylus.hungergameslobby.game.Scorebord;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RunnablesManager {
     PregameRunnable pregameRunnable;
+    GameRunnable gameRunnable;
+    Map<String, Object> runnablesMap = new HashMap<>();
     int gameNumber = 0;
 
-    public RunnablesManager(Game gameInstance){
+    public RunnablesManager(Game gameInstance, Scorebord scoreboardInstance){
         pregameRunnable = new PregameRunnable();
+        runnablesMap.put("pregameRunnable", pregameRunnable);
+        gameRunnable = new GameRunnable(gameInstance, scoreboardInstance);
+        runnablesMap.put("gameRunnable", gameRunnable);
+    }
+
+    public Object getRunnable(String runnableName){
+        return runnablesMap.get(runnableName);
     }
 }
