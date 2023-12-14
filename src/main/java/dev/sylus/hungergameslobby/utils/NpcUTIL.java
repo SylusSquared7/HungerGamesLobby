04fa50f60b8c5e910a765397e132f588613ce314
SylusSquared7 <sylussquared@proton.me>
2023-12-14 20:53:38 +0000

    Added the start of the leaderboard NPC's

--- src/main/java/dev/sylus/hungergameslobby/utils/NpcUTIL.java
+++ src/main/java/dev/sylus/hungergameslobby/utils/NpcUTIL.java
@@ -4,6 +4,9 @@ import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.npc.NPC;
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
+
+import java.util.logging.Level;
+
 public class NpcUTIL {
     public NpcUTIL(){ //Constructor
 
@@ -11,5 +14,6 @@ public class NpcUTIL {
 
     public void createNPC(String name, Location location){
         NPC npc = CitizensAPI.getNPCRegistry().createNPC(EntityType.PLAYER, name); npc.spawn(location);
+        Logging.log(Level.INFO, "Created an NPC with the name of: " + name + " and the location: " + location);
     }
 }
